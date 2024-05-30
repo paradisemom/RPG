27336d0803f4f59811ba0b9a203196bf8af2b399
paradisemom <andy774583558@gmail.com>
2024-05-30 12:05:25 +0800

    destroct

---
+++ RPG TOP DOWN/Assets/scripts/Destructible.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    [SerializeField] private GameObject destroyVFX;
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.GetComponent<DamegeSource>()){
+            Instantiate(destroyVFX,transform.position,Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
