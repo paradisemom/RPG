d509995c2f79e2f086c2f76c653f36cecdbf6374
paradisemom <andy774583558@gmail.com>
2024-06-24 17:05:48 +0800

    bow staff

--- RPG TOP DOWN/Assets/scripts/UI/Bow.cs
+++ RPG TOP DOWN/Assets/scripts/UI/Bow.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Bow : MonoBehaviour,IWeapon
 {
+
     public void Attack(){
         Debug.Log("Bow");
         ActiveWeapon.Instance.ToggleIsAttacking(false);
     }
+
 }
